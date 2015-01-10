5f30596139e4907f51cee47599fd07e9cbcc19b7
UpInTheAir <upintheair.xda@gmail.com>
2015-01-10 19:19:23 +0700

    cpufreq: exynos: incr KFC max voltage +25 mV for stabilty

--- drivers/cpufreq/exynos-mp-cpufreq.c
+++ drivers/cpufreq/exynos-mp-cpufreq.c
@@ -1381,7 +1381,7 @@ static size_t get_freq_table_size(struct cpufreq_frequency_table *freq_table)
 }
 
 #ifdef CONFIG_SOC_EXYNOS5433
-#define KFC_MAX_VOLT 1200000
+#define KFC_MAX_VOLT 1225000
 #define EGL_MAX_VOLT 1300000
 #else
 #warning "Please define core maximum voltages for current SoC."
