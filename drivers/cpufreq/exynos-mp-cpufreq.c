763dba1ebaac03f4f14507a1d6556d1aafb0385d
UpInTheAir <upintheair.xda@gmail.com>
2015-03-11 19:14:42 +0700

    cpufreq: exynos: increase EGL max voltage

--- drivers/cpufreq/exynos-mp-cpufreq.c
+++ drivers/cpufreq/exynos-mp-cpufreq.c
@@ -1382,7 +1382,7 @@ static size_t get_freq_table_size(struct cpufreq_frequency_table *freq_table)
 
 #ifdef CONFIG_SOC_EXYNOS5433
 #define KFC_MAX_VOLT 1225000
-#define EGL_MAX_VOLT 1300000
+#define EGL_MAX_VOLT 1312500
 #else
 #warning "Please define core maximum voltages for current SoC."
 #define KFC_MAX_VOLT 1300000
